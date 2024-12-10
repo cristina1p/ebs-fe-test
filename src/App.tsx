b489e5261d14ed6e7b5b52520d9935d6d4bc07ad
cristina <c1plamadeala@gmail.com>
2024-12-11 01:57:52 +0200

    Add router with two routes - home & shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import HomePage from './HomePage.tsx'
+import ShoppingCartPage from './ShoppingCartPage.tsx'
+import Layout from './Layout.tsx'
+import NotFoundPage from './NotFoundPage.tsx'
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to the Front-End Developer Test!</h1>
-      <p>We wish you the best of luck. Please make sure to read the README file for
-        instructions.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="cart" element={<ShoppingCartPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>Products</h1>
+}
+
+export default HomePage
