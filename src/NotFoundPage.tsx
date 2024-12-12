0cb90a0e51e4d407dd4e1f21eac14e6dcd881205
cristina <c1plamadeala@gmail.com>
2024-12-12 23:09:34 +0200

    Change Not Found page color

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 function NotFoundPage() {
   return (
-    <div className="flex flex-grow items-center justify-center bg-gray-100">
+    <div className="flex flex-grow items-center justify-center">
       <div className="text-center">
         <h1 className="text-6xl font-extrabold text-red-600">404</h1>
-        <p className="text-2xl text-gray-700 mt-2">Oops! Page not found.</p>
-        <p className="text-lg text-gray-500 mt-4">
+        <p className="text-2xl mt-2">Oops! Page not found.</p>
+        <p className="text-lg mt-4">
           The page you are looking for might have been moved or deleted.
         </p>
         <Link
